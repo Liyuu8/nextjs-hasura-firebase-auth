df831bc1b08a41ecd276d637d5b3133ec38f2e4c
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2021-08-29 19:31:18 +0900

    Use redux-toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
 
 import '../styles/globals.css'
 import { useUserChanged } from '../hooks/useUserChanged'
+import { store } from '../app/store'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const {} = useUserChanged()
 
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
