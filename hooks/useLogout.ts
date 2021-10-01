cc3aabac2e4f4d72ac280bb1a7df55062779f307
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2021-10-01 19:16:58 +0900

    Delete the cache and store at logout

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -1,16 +1,31 @@
 import Cookie from 'universal-cookie'
+import { useDispatch } from 'react-redux'
+import { useQueryClient } from 'react-query'
 
 import firebase from '../firebaseConfig'
 import { unSubMeta } from './useUserChanged'
+import { resetEditedNews, resetEditedTask } from '../slices/uiSlice'
 
 const cookie = new Cookie()
 
 export const useLogout = () => {
+  const dispatch = useDispatch()
+  const queryClient = useQueryClient()
+
   const logout = async () => {
-    if (unSubMeta) {
-      unSubMeta()
-    }
+    if (unSubMeta) unSubMeta()
+
     await firebase.auth().signOut()
+
+    // ストアをクリア
+    dispatch(resetEditedNews())
+    dispatch(resetEditedTask())
+
+    // キャッシュをクリア
+    queryClient.removeQueries('news')
+    queryClient.removeQueries('tasks')
+
+    // クッキーをクリア
     cookie.remove('token')
   }
 
